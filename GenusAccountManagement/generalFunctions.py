5e706ee4cece2e49b565994cef8a6d8840d7bc7a
matsteele <matsteele@gmail.com>
2018-07-16 19:59:38 -0400

    update of accounts 20180716

--- GenusAccountManagement/generalFunctions.py
+++ GenusAccountManagement/generalFunctions.py
@@ -16,25 +16,13 @@ def copy(data):
 def gitPush():
     date = datetime.date.today().strftime('%Y%m%d')
     script_dir = os.path.dirname(__file__)
-    script_dir = "'"+script_dir+"'"
-    script_dir = script_dir.replace("/Users/matthewsteele 1/","")
     print(script_dir)
-    orient = "pwd"
-    test = "echo 'testing'"
-    add = "git "
-    #script_dir = "'Desktop/ProjectFolder/GenusAccountManagement/GenusAccountManagement'"
-
-    commitDescrip = "'update of accounts {}'".format(date)
-    push = 'git push origin master"'
-    # os.system("echo '%s' | pbcopy" % data)
-    # subprocess.check_call(["ls", "-l"])
-    # os.system("cd  '%s' " % script_dir)
-    # subprocess.check_call(["echo",script_dir])
+    commitDescrip = "update of accounts {}".format(date)
+    subprocess.call(["pwd"])
     subprocess.call(["git","add","."])
     subprocess.call(["git","commit","-m",commitDescrip])
     subprocess.call(["git","push","origin","master"])
     #subprocess.call(["cd",script_dir])
     # subprocess.check_call([orient])
 # cd /Users/matthewsteele\ 1/Desktop/ProjectFolder/GenusAccountManagement/GenusAccountManagement
-
 gitPush()

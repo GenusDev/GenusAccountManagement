c3570142a00cf4ed782837d136a2dec3b8f6602c
matsteele <matsteele@gmail.com>
2018-08-03 18:07:30 -0400

    started account creation script

---
+++ programFiles/createAccounts.py
@@ -0,0 +1,16 @@
+
+
+print("Will initialize account creation")
+
+firstLetter = input("What is the first letter of your first name?")
+
+lastName = input("What is your last name?")
+
+accountName = concaten(firstLetter,lastName)
+
+accountData = {
+
+
+}
+
+#input to ask
